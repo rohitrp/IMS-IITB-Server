a8c94990db2ffb1b13ba1dc5625eb4e6fa8a2713
Rohit Prasad <rohitrp3@gmail.com>
2018-04-14 23:24:54 +0530

    Implement signup API

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from pymongo import MongoClient
+import json
+from bson.json_util import loads, dumps
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,12 +19,33 @@ class Login(Resource):
 
         user = users.find_one({'email': email})
         print(user)
-        if user and user.password == password:
-            return "SUCCESS"
+        if user and user['password'] == password:
+            return {'status': 1, 'email': user['email'], 'name': user['name']}
         else:
-            return "FAILED"
+            return {'status': 0}
+
+class Signup(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', type=str)
+    parser.add_argument('password', type=str)
+    parser.add_argument('name', type=str)
+
+    def post(self):
+        data = self.parser.parse_args()
+
+        print(data)
+        users = db.users
+
+        user = users.find_one({'email': data['email']})
+
+        if user is None:
+            user = users.insert_one(data)
+            return {'status': 1, 'email': data['email']}
+        else:
+            return {'status': 0}
 
 api.add_resource(Login, '/api/login/')
+api.add_resource(Signup,'/api/signup/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
